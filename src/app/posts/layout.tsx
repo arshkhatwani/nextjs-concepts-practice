6308939cda2535296776166e83504d42cd9a5ab6
arshkhatwani <arsh.khatwani@gmail.com>
2023-11-16 23:54:46 +0530

    Adds metadata to pages and layouts

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Posts",
+  description: "Posts",
+};
+
 export default function PostLayout({
   children,
 }: {
--- src/app/posts/new/page.tsx
+++ src/app/posts/new/page.tsx
@@ -1,4 +1,10 @@
 import Heading from "@/components/Heading";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "New Post",
+  description: "Add a new post",
+};
 
 export default function NewPost() {
   return <Heading>New Post</Heading>;
