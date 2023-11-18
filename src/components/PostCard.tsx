656c4ac7634de2007da68dd8652acca2324a2c08
arshkhatwani <arsh.khatwani@gmail.com>
2023-11-18 13:23:14 +0530

    Updates PostCard component UI

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,11 +1,11 @@
 import { Post } from "@prisma/client";
 
-export default function PostCard({ id, title, description }: Post) {
+export default function PostCard({ title, description }: Post) {
     return (
-        <div>
-            <h1>{title}</h1>
+        <div className="border-2 border-gray-400 rounded-lg p-5 my-3">
+            <h1 className="text-3xl font-semibold mb-3">{title}</h1>
 
-            <p>{description}</p>
+            <p className="text-2xl font-medium text-gray-600">{description}</p>
         </div>
     );
 }
