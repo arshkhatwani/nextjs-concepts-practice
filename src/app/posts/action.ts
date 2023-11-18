a4d1ccc4a974dcba5f8425d39b2df8ff87ab74f0
arshkhatwani <arsh.khatwani@gmail.com>
2023-11-18 16:26:35 +0530

    Adds cache limit

--- src/app/posts/action.ts
+++ src/app/posts/action.ts
@@ -21,7 +21,11 @@ export async function getPosts() {
 
 export async function getAllPosts() {
     const API_URL = "https://dummyjson.com/posts";
-    const data = await fetch(API_URL);
+    const data = await fetch(API_URL, {
+        next: {
+            revalidate: 30, // will fetch again after 30 seconds
+        },
+    });
     const posts = await data.json();
     return posts.posts;
 }
