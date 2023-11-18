2d207af117daf5a5e4c0673bd081a34fd73a793e
arshkhatwani <arsh.khatwani@gmail.com>
2023-11-18 13:30:42 +0530

    Updates savePost function

--- src/app/posts/action.ts
+++ src/app/posts/action.ts
@@ -1,12 +1,17 @@
 "use server";
 
 import prisma from "@/db";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function savePost(formData: FormData) {
     const title = formData.get("title") as string;
     const description = formData.get("description") as string;
 
     await prisma.post.create({ data: { title, description } });
+
+    revalidatePath("/posts");
+    redirect("/posts");
 }
 
 export async function getPosts() {
