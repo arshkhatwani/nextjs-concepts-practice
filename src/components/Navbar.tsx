a2306fcc9adcc4086d297a60de19f4e004629596
arshkhatwani <arsh.khatwani@gmail.com>
2023-11-19 16:17:51 +0530

    Adds image

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const linkClass = "text-xl text-white hover:text-blue-700";
@@ -10,7 +11,14 @@ const navItems = [
 
 const Navbar = () => {
     return (
-        <div className="flex justify-end py-4 px-3 gap-10 bg-slate-700">
+        <div className="flex justify-end py-4 px-3 items-center gap-10 bg-slate-700 relative">
+            <Image
+                src={"/favicon.ico"}
+                alt="icon"
+                width={30}
+                height={30}
+                className="absolute left-5"
+            />
             {navItems.map((item) => (
                 <Link key={item.name} href={item.path} className={linkClass}>
                     {item.name}
